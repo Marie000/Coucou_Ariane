216823266a8d583ea866516a2b0cbf69c69e5893
Marie <marie.pelletier@gmail.com>
2015-12-18 09:04:27 -0500

    optimized the layout to fit a 7 kids tablet

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ document.getElementById('main').style.visibility="visible";
 document.getElementById('encore').style.visibility="hidden";
 var element = document.getElementById('jouer');
 element.parentNode.removeChild(element);
-}
+};
 
 //initial variables + getRandom function
-var people = ['lili','tessa','pepe','grand-maman','grand-papa','vincent','philippe','genevieve','alexis','laurence']
+var people = ['lili','tessa','pepe','grand-maman','grand-papa','vincent','philippe','genevieve','alexis','laurence'];
 var copyOfPeople = people.slice();
 
 var newPerson;
@@ -18,7 +18,7 @@ var getRandom = function(){
   newPerson = people[Math.floor(Math.random()*people.length)];
   var index = people.indexOf(newPerson);
   people.splice(index, 1);
-}
+};
 getRandom();
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","-"]
 var next;
